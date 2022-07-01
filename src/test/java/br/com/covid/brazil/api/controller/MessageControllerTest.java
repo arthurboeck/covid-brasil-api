1170a732b1369b0ceced575c7f984fb86d7db1fb
arthur_boeck <#Menezes5>
2022-07-01 19:12:03 -0300

    fix teste controller

--- src/test/java/br/com/covid/brazil/api/controller/MessageControllerTest.java
+++ src/test/java/br/com/covid/brazil/api/controller/MessageControllerTest.java
@@ -2,7 +2,6 @@ package br.com.covid.brazil.api.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,7 +10,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
 @WebMvcTest(controllers = MessageController.class)
 class MessageControllerTest {
 
