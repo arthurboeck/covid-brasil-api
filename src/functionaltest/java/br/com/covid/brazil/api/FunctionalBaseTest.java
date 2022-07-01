5dda81e528331d3c8b790b7432e2f2fcb70c8fb0
arthur_boeck <#Menezes5>
2022-06-30 21:46:49 -0300

    ajustes testes unitarios

--- src/functionaltest/java/br/com/covid/brazil/api/FunctionalBaseTest.java
+++ src/functionaltest/java/br/com/covid/brazil/api/FunctionalBaseTest.java
@@ -1,14 +1,16 @@
 package br.com.covid.brazil.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles("it")
+@AutoConfigureMockMvc
 @AutoConfigureWireMock(port = 0, stubs = "classpath*:/mappings/**/*.json")
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FunctionalBaseTest {
 
     @Autowired
