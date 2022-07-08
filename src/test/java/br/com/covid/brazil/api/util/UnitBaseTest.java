2a66440984551da99874257b80f5ec375a0c2c16
arthur_boeck <#Menezes5>
2022-07-08 17:50:07 -0300

    remocao de metodo nao utilizado

--- src/test/java/br/com/covid/brazil/api/util/UnitBaseTest.java
+++ src/test/java/br/com/covid/brazil/api/util/UnitBaseTest.java
@@ -3,10 +3,7 @@ package br.com.covid.brazil.api.util;
 import br.com.covid.brazil.api.dto.CovidDataDTO;
 import br.com.covid.brazil.api.model.CovidData;
 import org.springframework.test.context.ActiveProfiles;
-import wiremock.com.fasterxml.jackson.annotation.JsonInclude;
-import wiremock.com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -19,10 +16,4 @@ public class UnitBaseTest {
     public CovidData retornoCovidDataModel =
             new CovidData(1, "RS", "Alegrete", 123L, 123L, LocalDate.now(), "202212", 1222L, 12000L, 100L, BigDecimal.ONE, LocalDate.now(), BigDecimal.TEN, 1L, 2L, 3L);
 
-    public String toJson(Object object) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        return mapper.writeValueAsString(object);
-    }
 }
