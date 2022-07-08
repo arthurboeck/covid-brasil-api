d26d48f04d5a304fdf3c2e245efd50a3dfec41aa
arthur_boeck <#Menezes5>
2022-07-08 17:53:22 -0300

    add estrutura funcional

---
+++ src/functionaltest/java/br/com/covid/brazil/api/util/RotasEnum.java
@@ -0,0 +1,17 @@
+package br.com.covid.brazil.api.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RotasEnum {
+
+    OBTER_DADOS_COVID_BRASIL_IO("/api/v1/covid/externo"),
+    PERSISTIR_DADOS_COVID_BRASIL_IO("/api/v1/covid"),
+    LISTAR_TODAS_CONSULTAS("/api/v1/covid"),
+    LISTAR_CONSULTA_BY_ID("/api/v1/covid/{id}"),
+    DELETE_CONSULTA_BY_ID("/api/v1/covid/{id}");
+
+    private final String url;
+}
---
+++ src/functionaltest/java/br/com/covid/brazil/api/util/UtilFunctionalConstantes.java
@@ -0,0 +1,26 @@
+package br.com.covid.brazil.api.util;
+
+public class UtilFunctionalConstantes {
+    public static final String KEY_ID = "{id}";
+
+    public static final String ID_PARAM_BODY = "id";
+    public static final String UF_PARAM_BODY = "uf";
+    public static final String MUNICIPIO_PARAM_BODY = "municipio";
+    public static final String CODIGO_IBGE_MUNICIPIO_BODY = "codigoIbgeMunicipio";
+    public static final String RANKING_MUNICIPIO_BODY = "rankingMunicipio";
+    public static final String DATA_COLETA_DADOS_BODY = "dataColetaDados";
+    public static final String SEMANA_EPIDEMIOLOGICA_BODY = "semanaEpidemiologica";
+    public static final String POPULACAO_ESTIMADA_BODY = "populacaoEstimada";
+    public static final String POPULACAO_ESTIMADA_2019_BODY = "populacaoEstimada2019";
+    public static final String CASOS_CONFIRMADOS_ULTIMO_DIA_BODY = "casosConfirmadosUltimoDia";
+    public static final String CASOS_CONFIRMADOS_ULTIMO_DIA_POR_100_K_HABITANTES_BODY = "casosConfirmadosUltimoDiaPor100kHabitantes";
+    public static final String DATA_DADO_BODY = "dataDado";
+    public static final String TAXA_MORTALIDADE_ULTIMO_DIA_BODY = "taxaMortalidadeUltimoDia";
+    public static final String MORTES_ULTIMO_DIA_BODY = "mortesUltimoDia";
+    public static final String NOVOS_CASOS_BODY = "novosCasos";
+    public static final String NOVAS_MORTES_BODY = "novasMortes";
+
+    public static final String ALEGRE_PARAM = "Alegrete";
+    public static final String RS_PARAM = "RS";
+
+}
