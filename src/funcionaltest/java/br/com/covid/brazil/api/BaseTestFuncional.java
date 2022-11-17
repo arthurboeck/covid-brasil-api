daad0d35f6536aeedfe5fa1d2f10ce86578be190
arthur_boeck <arthur_boeck@sicredi.com.br>
2022-11-16 23:34:26 -0300

    refactory e criacao de novos testes funcionais

--- src/funcionaltest/java/br/com/covid/brazil/api/BaseTestFuncional.java
+++ src/funcionaltest/java/br/com/covid/brazil/api/BaseTestFuncional.java
@@ -4,6 +4,7 @@ import br.com.covid.brazil.api.dto.CovidDataDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,7 +15,7 @@ import java.time.LocalDate;
 @AutoConfigureMockMvc
 @ActiveProfiles("funcional")
 @AutoConfigureWireMock(port = 0, stubs = "classpath*:/mappings/**/*.json")
-@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = {Application.class}, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BaseTestFuncional {
 
     @Autowired
@@ -25,7 +26,10 @@ public class BaseTestFuncional {
     }
 
     public CovidDataDTO retornoSucesso =
-            new CovidDataDTO(null, "RS", "Alegrete", 4300406L, 723L, LocalDate.of(2022, 3, 24), "202212", 73028L, 73589L, 15009L, BigDecimal.valueOf(20552.39086), LocalDate.of(2022, 3, 24), BigDecimal.valueOf(0.0216), 324L, 36L, 0L);
-
+            new CovidDataDTO(null, "RS", "Alegrete", 4300406L, 723L,
+                    LocalDate.of(2022, 3, 24), "202212", 73028L,
+                    73589L, 15009L, BigDecimal.valueOf(20552.39086),
+                    LocalDate.of(2022, 3, 24), BigDecimal.valueOf(0.0216), 324L,
+                    36L, 0L);
 }
 
